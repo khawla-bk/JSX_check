2b46e02820345eafd530fc0775f519deb637e21c
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-06-11 14:00:16 +0100

    Add files via upload

---
+++ jsxcheckpoint/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import imageInSrc from "./imageInSrc.png";
+import "./Style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className={"title red"}> Your name here </h1>
+        <div>
+          {" "}
+          <img src={imageInSrc} alt="photosrc" />{" "}
+        </div>
+        <img src="/imageInPublic.jpg" alt="photopublic" />
+      </div>
+      <video width="320" height="240" controls>
+        <source src="/myVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
